693dfcc412216ce60366bd9a046ca4488d4a1b6c
Michael <none@none.org>
2017-11-04 17:53:38 -0500

    Include _id in schema

--- app/models/story.js
+++ app/models/story.js
@@ -2,12 +2,12 @@
 var mongoose = require('mongoose');
 
 var storySchema = new mongoose.Schema({
-	// creator: {type: Schema.Types.ObjectId, ref: 'User'},
-
-	title: String,
-	content: String,
-	prevContent: Number,
-	comment: String
-})
+  // creator: {type: Schema.Types.ObjectId, ref: 'User'},
+  _id: Number,
+  title: String,
+  content: String,
+  prevContent: Number,
+  comment: String,
+});
 
 module.exports = mongoose.model('Story', storySchema);
