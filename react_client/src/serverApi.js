b1637e882f977e02ae8795859fe676c3107bf274
Michael <none@none.org>
2017-11-04 17:42:54 -0500

    Copy _id as id

--- react_client/src/serverApi.js
+++ react_client/src/serverApi.js
@@ -2,5 +2,6 @@ export const fetchStories = () => fetch('/api/profile').then(res => res.json());
 export const fetchStory = id =>
   fetchStories()
     .then(result => result.stories)
+    .then(result => result.map(story => ({ ...story, id: story._id })))
     .then(result => result.filter(story => story.id === id))
     .then(result => (result.length === 1 ? result[0] : null));
