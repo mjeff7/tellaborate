ec615a0f61ae1befffb65d093b430316b2eb9e8b
Michael <none@none.org>
2017-11-04 17:06:35 -0500

    Add server API

---
+++ react_client/src/serverApi.js
@@ -0,0 +1,5 @@
+export const fetchStories = () => fetch('/api/profile').then(res => res.json());
+export const fetchStory = id =>
+  fetchStories()
+    .then(result => result.filter(story => story.id === id))
+    .then(result => (result.length === 1 ? result[0] : null));
