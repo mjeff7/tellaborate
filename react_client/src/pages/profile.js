018bbb2cc77f9d123224140da82b823426abb27e
Michael <none@none.org>
2017-11-04 13:56:50 -0500

    Add slim copy of profile page from designer template

---
+++ react_client/src/pages/profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProfileStoryView = () => (
+  <div className="col-md-4 col-sm-6 col-xs-6 col-xxs-12 work-item">
+    <a href="work.html">
+      <img
+        src="images/work_1.jpg"
+        alt="Free HTML5 Website Template by FreeHTML5.co"
+        className="img-responsive"
+      />
+      <h3 className="fh5co-work-title">Game of Thrones S8</h3>
+      <p>Decide the fate of Westeros!</p>
+    </a>
+  </div>
+);
+
+export default () => null;
