b77a136ddf00ab57a726b81117483aab6c215c54
Michael <none@none.org>
2017-11-04 17:38:12 -0500

    Use server api correctly

--- react_client/src/serverApi.js
+++ react_client/src/serverApi.js
@@ -1,8 +1,6 @@
-export const fetchStories = () =>
-  fetch('/api/profile').then(
-    res => (res != null ? res.json() : console.error('Fetch error') || [])
-  );
+export const fetchStories = () => fetch('/api/profile').then(res => res.json());
 export const fetchStory = id =>
   fetchStories()
+    .then(result => result.stories)
     .then(result => result.filter(story => story.id === id))
     .then(result => (result.length === 1 ? result[0] : null));
