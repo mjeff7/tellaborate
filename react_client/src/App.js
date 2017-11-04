36b9f2c8096eb94240cc1277c33c650f9613e3b2
Michael <none@none.org>
2017-11-04 14:37:59 -0500

    Update profile page to full template

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Profile from './pages/Profile';
 
 const Landing = () => null;
 
 const App = () => (
   <BrowserRouter>
-    <Route path="/landing" component={Landing} />
+    <div>
+      <Route path="/profile" component={Profile} />
+      <Route path="/landing" component={Landing} />
+    </div>
   </BrowserRouter>
 );
 
